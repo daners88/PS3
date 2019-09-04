51e7fbc62548608282aab579a02e55e0962b85f8
Dane Carter <u0645467@ad.utah.edu>
2019-09-03 20:37:53 -0600

    fixing code

--- Assets/Scripts/Editor/QuadTileBoardPCGInspector.cs
+++ Assets/Scripts/Editor/QuadTileBoardPCGInspector.cs
@@ -26,47 +26,17 @@ public class QuadTileBoardPCGInspector : Editor
 
         if (GUILayout.Button("Create Map"))
         {
-            DrunkenWalk(pcg, pcg.tilePrefab, Vector3.zero, new List<Vector3>(){
+            pcg.StartCoroutine(pcg.DrunkenWalk(pcg, pcg.tilePrefab, Vector3.zero, new List<Vector3>(){
                 Vector3.forward,
                 Vector3.back,
                 Vector3.right,
                 Vector3.left },
                 pcg.stepCount,
-                pcg.walkCount);
+                pcg.walkCount));
         }
         EditorGUILayout.EndHorizontal();
 
         pcg.wanderWalk = EditorGUILayout.Toggle("Wander", pcg.wanderWalk);
         base.OnInspectorGUI();
-    }
-
-    void DrunkenWalk(QuadTileBoardPCG pcg, GameObject prefab, Vector3 start, List<Vector3> directions, int stepCount, int walkCount = 1)
-    {
-        System.Random seedGenerator = new System.Random();
-        System.Random rnd = new System.Random(seedGenerator.Next());
-
-        List<GameObject> tiles = new List<GameObject>();
-
-        for(int j = 0; j < walkCount; j++)
-        {
-            Vector3 curPoint = Vector3.zero;
-            if (pcg.wanderWalk)
-            {
-                curPoint = tiles.Count == 0 ? start : tiles[rnd.Next(0, tiles.Count)].transform.position;
-            }
-            else
-            {
-                curPoint = start;
-            }
-            
-            for (int i = 0; i < stepCount; i++)
-            {
-                GameObject tile = Instantiate<GameObject>(prefab, curPoint, Quaternion.identity, pcg.transform);
-                tiles.Add(tile);
-                int dirIndex = rnd.Next(0, directions.Count);
-                Vector3 dir = directions[dirIndex];
-                curPoint += dir;
-            }
-        }
     }
 }
--- Assets/Scripts/QuadTileBoardPCG.cs
+++ Assets/Scripts/QuadTileBoardPCG.cs
@@ -6,6 +6,56 @@ public class QuadTileBoardPCG : MonoBehaviour
 {
     public GameObject tilePrefab = null;
     public int stepCount = 10;
-    public int walkCount = 10;
+    public int walkCount = 1;
     public bool wanderWalk = false;
+
+    IEnumerator LerpObj(Transform target, Vector3 start, Vector3 end, float duration)
+    {
+        float time = 0;
+        while(time < duration)
+        {
+            float t = time / duration;
+            target.position = Vector3.Lerp(start, end, t);
+
+            yield return new WaitForEndOfFrame();
+            time += Time.deltaTime;
+        }
+    }
+
+    public void DrunkenWalk()
+    {
+        //start coroutine using local variables
+    }
+
+    //use while loop to reach a number of tiles
+    public IEnumerator DrunkenWalk(QuadTileBoardPCG pcg, GameObject prefab, Vector3 start, List<Vector3> directions, int stepCount, int walkCount = 1)
+    {
+        System.Random seedGenerator = new System.Random();
+        System.Random rnd = new System.Random(seedGenerator.Next());
+
+        List<GameObject> tiles = new List<GameObject>();
+
+        for (int j = 0; j < walkCount; j++)
+        {
+            Vector3 curPoint = Vector3.zero;
+            if (pcg.wanderWalk)
+            {
+                curPoint = tiles.Count == 0 ? start : tiles[rnd.Next(0, tiles.Count)].transform.position;
+            }
+            else
+            {
+                curPoint = start;
+            }
+
+            for (int i = 0; i < stepCount; i++)
+            {
+                GameObject tile = Instantiate<GameObject>(prefab, curPoint, Quaternion.identity, pcg.transform);
+                tiles.Add(tile);
+                int dirIndex = rnd.Next(0, directions.Count);
+                Vector3 dir = directions[dirIndex];
+                curPoint += dir;
+                yield return null;
+            }
+        }
+    }
 }
