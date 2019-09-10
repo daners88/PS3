bca4899f127d6e16a65dbbe8842924928817ce21
Dane <daners88@yahoo.com>
2019-09-10 09:36:19 -0600

    generates ocean and is a bit faster

--- Assets/Scripts/Editor/QuadTileBoardPCGInspector.cs
+++ Assets/Scripts/Editor/QuadTileBoardPCGInspector.cs
@@ -6,6 +6,11 @@ using UnityEditor;
 [CustomEditor(typeof(QuadTileBoardPCG))]
 public class QuadTileBoardPCGInspector : Editor
 {
+    static IEnumerator thingy = null;
+    private void OnEnable()
+    {
+        EditorApplication.update += InEditorGenerate;
+    }
 
     public override void OnInspectorGUI()
     {
@@ -19,19 +24,34 @@ public class QuadTileBoardPCGInspector : Editor
         }
 
         pcg.tilePrefab = EditorGUILayout.ObjectField("Tile Prefab", pcg.tilePrefab, typeof(GameObject), true) as GameObject;
+        pcg.waterPrefab = EditorGUILayout.ObjectField("Water Prefab", pcg.waterPrefab, typeof(GameObject), true) as GameObject;
         pcg.placementCounter = EditorGUILayout.ObjectField("Placement Count UI Object", pcg.placementCounter, typeof(GameObject), true) as GameObject;
+        pcg.timer = EditorGUILayout.ObjectField("Timer UI Object", pcg.timer, typeof(GameObject), true) as GameObject;
         EditorGUILayout.BeginHorizontal();
         pcg.seed = EditorGUILayout.IntField(pcg.seed);
         pcg.stepCount = EditorGUILayout.IntField(pcg.stepCount);
         pcg.walkCount = EditorGUILayout.IntField(pcg.walkCount);
+        pcg.waterBuffer = EditorGUILayout.IntField(pcg.waterBuffer);
 
         if (GUILayout.Button("Create Map"))
         {
-            pcg.DrunkenWalk();
+            thingy = pcg.DrunkenWalk();
         }
         EditorGUILayout.EndHorizontal();
 
         pcg.wanderWalk = EditorGUILayout.Toggle("Wander", pcg.wanderWalk);
         base.OnInspectorGUI();
+    }
+
+    private void InEditorGenerate()
+    {
+        if(thingy != null)
+        {
+            if (!thingy.MoveNext())
+            {
+                thingy = null;
+            }
+        }
+        
     }
 }
--- Assets/Scripts/QuadTileBoardPCG.cs
+++ Assets/Scripts/QuadTileBoardPCG.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class QuadTileBoardPCG : MonoBehaviour
 {
     public GameObject tilePrefab = null;
+    public GameObject waterPrefab = null;
+    public int waterBuffer = 5;
     public int stepCount = 10;
     public int walkCount = 1;
     public int seed = 1;
@@ -13,11 +15,20 @@ public class QuadTileBoardPCG : MonoBehaviour
     public bool randomSeed = false;
     private float minX = 0, minZ = 0, maxX = 0, maxZ = 0;
     public GameObject placementCounter;
+    public GameObject timer;
     private UnityEngine.UI.Text counterText;
+    private UnityEngine.UI.Text timeElapsed;
+    private int indexAtWhichThingsBecomeWater = 0;
+    List<Vector3> directions = new List<Vector3>(){
+                Vector3.forward,
+                Vector3.back,
+                Vector3.right,
+                Vector3.left };
 
     private void Start()
     {
         counterText = placementCounter.GetComponent<UnityEngine.UI.Text>();
+        timeElapsed = timer.GetComponent<UnityEngine.UI.Text>();
     }
 
     IEnumerator LerpObj(Transform target, Vector3 start, Vector3 end, float duration)
@@ -33,19 +44,52 @@ public class QuadTileBoardPCG : MonoBehaviour
         }
     }
 
-    public void DrunkenWalk()
+    public struct IntKey
     {
-        StartCoroutine(DrunkenWalk(Vector3.zero, new List<Vector3>(){
-                Vector3.forward,
-                Vector3.back,
-                Vector3.right,
-                Vector3.left }));
+        private readonly int first;
+        private readonly int second;
+
+        public int First { get { return first; } }
+        public int Second { get { return second; } }
+
+        public IntKey(int first, int second)
+        {
+            this.first = first;
+            this.second = second;
+        }
+
+        public bool Equals(IntKey other)
+        {
+            return this.first == other.first && this.second == other.second;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+            return obj is IntKey && Equals((IntKey)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (this.first * 397) ^ this.second;
+            }
+        }
     }
 
-    public float SetHeight(System.Random rnd, Vector3 curPoint, List<GameObject> tiles)
+    public void StartDrunkenWalk()
+    {
+        StartCoroutine(DrunkenWalk());
+    }
+
+    public float SetHeight(System.Random rnd, Vector3 curPoint, Dictionary<IntKey, GameObject> tileDictionary)
     {
         int determinator = rnd.Next(0, 100);
-        if(determinator > 60)
+        if(determinator > 75)
         {
             return curPoint.y;
         }
@@ -53,14 +97,14 @@ public class QuadTileBoardPCG : MonoBehaviour
         {
             float minY = 5.0f, maxY = 0.0f;
             List<GameObject> possibleAdjacents = new List<GameObject>();
-            possibleAdjacents.Add(tiles.Where(t => t.transform.position.x == curPoint.x - 1 && t.transform.position.z == curPoint.z + 1).FirstOrDefault());
-            possibleAdjacents.Add(tiles.Where(t => t.transform.position.x == curPoint.x && t.transform.position.z == curPoint.z + 1).FirstOrDefault());
-            possibleAdjacents.Add(tiles.Where(t => t.transform.position.x == curPoint.x + 1 && t.transform.position.z == curPoint.z + 1).FirstOrDefault());
-            possibleAdjacents.Add(tiles.Where(t => t.transform.position.x == curPoint.x - 1 && t.transform.position.z == curPoint.z).FirstOrDefault());
-            possibleAdjacents.Add(tiles.Where(t => t.transform.position.x == curPoint.x + 1 && t.transform.position.z == curPoint.z).FirstOrDefault());
-            possibleAdjacents.Add(tiles.Where(t => t.transform.position.x == curPoint.x - 1 && t.transform.position.z == curPoint.z - 1).FirstOrDefault());
-            possibleAdjacents.Add(tiles.Where(t => t.transform.position.x == curPoint.x && t.transform.position.z == curPoint.z - 1).FirstOrDefault());
-            possibleAdjacents.Add(tiles.Where(t => t.transform.position.x == curPoint.x + 1 && t.transform.position.z == curPoint.z - 1).FirstOrDefault());
+            possibleAdjacents.Add(tileDictionary.Where(t => t.Key.Equals(new IntKey((int)curPoint.x, (int)curPoint.z + 1))).FirstOrDefault().Value);
+            possibleAdjacents.Add(tileDictionary.Where(t => t.Key.Equals(new IntKey((int)curPoint.x, (int)curPoint.z + 1))).FirstOrDefault().Value);
+            possibleAdjacents.Add(tileDictionary.Where(t => t.Key.Equals(new IntKey((int)curPoint.x, (int)curPoint.z + 1))).FirstOrDefault().Value);
+            possibleAdjacents.Add(tileDictionary.Where(t => t.Key.Equals(new IntKey((int)curPoint.x - 1, (int)curPoint.z))).FirstOrDefault().Value);
+            possibleAdjacents.Add(tileDictionary.Where(t => t.Key.Equals(new IntKey((int)curPoint.x + 1, (int)curPoint.z))).FirstOrDefault().Value);
+            possibleAdjacents.Add(tileDictionary.Where(t => t.Key.Equals(new IntKey((int)curPoint.x - 1, (int)curPoint.z - 1))).FirstOrDefault().Value);
+            possibleAdjacents.Add(tileDictionary.Where(t => t.Key.Equals(new IntKey((int)curPoint.x, (int)curPoint.z - 1))).FirstOrDefault().Value);
+            possibleAdjacents.Add(tileDictionary.Where(t => t.Key.Equals(new IntKey((int)curPoint.x + 1, (int)curPoint.z - 1))).FirstOrDefault().Value);
 
 
             foreach (var adj in possibleAdjacents)
@@ -115,9 +159,12 @@ public class QuadTileBoardPCG : MonoBehaviour
     }
 
     //use while loop to reach a number of tiles
-    public IEnumerator DrunkenWalk(Vector3 start, List<Vector3> directions)
+    public IEnumerator DrunkenWalk()
     {
+        Vector3 start = Vector3.zero;
+        int maxX = 0, minX = 0, maxZ = 0, minZ = 0;
         int placed = 0;
+        //float time = 0.0f;
         System.Random rnd;
         if (randomSeed)
         {
@@ -130,6 +177,8 @@ public class QuadTileBoardPCG : MonoBehaviour
         }
 
         List<GameObject> tiles = new List<GameObject>();
+
+        Dictionary<IntKey, GameObject> tileDictionary = new Dictionary<IntKey, GameObject>();
 
         for (int j = 0; j < walkCount; j++)
         {
@@ -146,14 +195,32 @@ public class QuadTileBoardPCG : MonoBehaviour
             for (int i = 0; i < stepCount; i++)
             {
                 int stepsWithoutPlacing = 0;
-                if (tiles.Where(t => t.transform.position.x == curPoint.x && t.transform.position.z == curPoint.z).FirstOrDefault() == null)
+                if (!tileDictionary.ContainsKey(new IntKey((int)curPoint.x, (int)curPoint.z)))
                 {
                     stepsWithoutPlacing = 0;
-                    curPoint.y = SetHeight(rnd, curPoint, tiles);
+                    curPoint.y = SetHeight(rnd, curPoint, tileDictionary);
                     GameObject tile = Instantiate<GameObject>(tilePrefab, curPoint, Quaternion.identity, transform);
                     tile.transform.localScale = new Vector3(1, curPoint.y * 2, 1);
                     tiles.Add(tile);
+                    tileDictionary.Add(new IntKey((int)curPoint.x, (int)curPoint.z), tile);
                     placed++;
+                    if(curPoint.x < minX)
+                    {
+                        minX = (int)curPoint.x;
+                    }
+                    else if (curPoint.x > maxX)
+                    {
+                        maxX = (int)curPoint.x;
+                    }
+                    if (curPoint.z < minZ)
+                    {
+                        minZ = (int)curPoint.z;
+                    }
+                    else if(curPoint.z > maxZ)
+                    {
+                        maxZ = (int)curPoint.z;
+                    }
+                    
                 }
                 else
                 {
@@ -167,9 +234,37 @@ public class QuadTileBoardPCG : MonoBehaviour
                 int dirIndex = rnd.Next(0, directions.Count);
                 Vector3 dir = directions[dirIndex];
                 curPoint += dir;
-                counterText.text = placed.ToString();
-                yield return null;
+                //counterText.text = placed.ToString();
+                //time += Time.deltaTime;
+                //timeElapsed.text = time.ToString();
+                
             }
+            yield return null;
         }
+
+        indexAtWhichThingsBecomeWater = tiles.Count - 1;
+
+        List<GameObject> FullMap = tiles;
+        for(int i = minX - waterBuffer; i <= maxX + waterBuffer; i++)
+        {
+            for(int j = minZ - waterBuffer; j <= maxZ + waterBuffer; j++)
+            {
+                if(i < minX || i > maxX || j < minZ || j > maxZ)
+                {
+                    GameObject water = Instantiate<GameObject>(waterPrefab, new Vector3(i, 0, j), Quaternion.identity, transform);
+                    FullMap.Add(water);
+                }
+                else if (!tileDictionary.ContainsKey(new IntKey(i, j)))
+                {
+                    GameObject water = Instantiate<GameObject>(waterPrefab, new Vector3(i, 0, j), Quaternion.identity, transform);
+                    FullMap.Add(water);
+                }
+                //time += Time.deltaTime;
+                //timeElapsed.text = time.ToString();
+                
+            }
+            yield return null;
+        }
+
     }
 }
